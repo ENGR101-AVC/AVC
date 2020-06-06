443969f75216e26241048f31deac9e41abb818a9
Dilitha Wijayapala <60630994+DilithaW123@users.noreply.github.com>
2020-06-06 16:17:30 +1200

    Refactored Core Code
    Should be same result but finding white pixels and driving car are in separate functions.

--- robot.cpp
+++ robot.cpp
@@ -1,57 +1,67 @@
 #include "robot.hpp"
 
+int getWhite(int whitePixArray[]){
+	int whiteRatio = 0;
+	for (int i=0;i<cameraView.width;i++){
+		int pix = get_pixel(cameraView,50,i,3);//getting the color value of the pixel (not individual colours)
+		int boolWhite=0;
+		if(pix==255){
+			boolWhite =1;
+		} 
+		else if(pix!=255){
+			boolWhite=0;
+		}
+		std::cout<<boolWhite<<" ";
+		whitePixArray[i]=boolWhite;
+	}
+	for (int n=0;n<cameraView.width;n++){
+		if(whitePixArray[n]==1){
+			whiteRatio=n; //finds first white pixel and exits for loop
+			return whiteRatio;
+		}	
+	}	
+	return 0;
+}
+
+void drive(int whiteRatio){
+	double vLeft = 30.0;
+	double vRight = 30.0;
+	if(whiteRatio<30){//treats each image as if it has 5 sections if pixel is not in the middle section robot moves accordingly
+		vLeft = 20;
+		vRight = 30;
+	}	
+	if((whiteRatio>30)&&(whiteRatio<30)){
+		vLeft = 20;
+		vRight = 25;
+	}	
+	if((whiteRatio>60)&&(whiteRatio<90)){//middle section of image
+		vLeft = 30;
+		vRight = 30;
+		}	
+	if((whiteRatio>90)&&(whiteRatio<120)){
+		vLeft = 25;
+		vRight = 20;
+	}	
+	if((whiteRatio>120)&&(whiteRatio<150)){
+		vLeft = 30;
+		vRight = 20;
+	}	
+     setMotors(vLeft,vRight);   
+     std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+	
+}	
+
 int main(){
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-    double vLeft = 40.0;
-    double vRight = 30.0;
     takePicture();
     SavePPMFile("i0.ppm",cameraView);
     int whitePixArray[cameraView.width];//array capacity of cameraWindow.width set because robot.fovWidth = 150.0 
     while(1){
 		takePicture();
-		int whiteRatio = 0;
-		for (int i=0;i<cameraView.width;i++){
-			int pix = get_pixel(cameraView,50,i,3);//getting the color value of the pixel (not individual colours)
-			int boolWhite;
-			if(pix==255){
-				boolWhite =1;
-			} 
-			else if(pix!=255){
-				boolWhite=0;
-			}
-			std::cout<<boolWhite<<" ";
-			whitePixArray[i]=boolWhite;
-		}
-		for (int n=0;n<150;n++){
-			if(whitePixArray[n]==1){
-				whiteRatio=n; //finds first white pixel and exits for loop
-				break;
-			}	
-		}	
-		if(whiteRatio<30){//treats each image as if it has 5 sections if pixel is not in the middle section robot moves accordingly
-			vLeft = 20;
-			vRight = 30;
-		}	
-		if((whiteRatio>30)&&(whiteRatio<30)){
-			vLeft = 20;
-			vRight = 25;
-		}	
-		if((whiteRatio>60)&&(whiteRatio<90)){//middle section of image
-			vLeft = 30;
-			vRight = 30;
-		}	
-		if((whiteRatio>90)&&(whiteRatio<120)){
-			vLeft = 25;
-			vRight = 20;
-		}	
-		if((whiteRatio>120)&&(whiteRatio<150)){
-			vLeft = 30;
-			vRight = 20;
-		}	
-      setMotors(vLeft,vRight);   
-      std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+		int whiteRatio = getWhite(whitePixArray);
+		drive(whiteRatio);
        usleep(10000);
   } //while
 
